261d4b98180f8c02fbc6bfd8d665de4c94f862ff
beratatmaca <berat.atmaca@redwirespaceeurope.com>
2024-01-31 09:38:09 +0100

    Adding the mpsoc definitions

--- toolchains/aarch64-linux-gnu.toolchain.cmake
+++ toolchains/aarch64-linux-gnu.toolchain.cmake
@@ -8,8 +8,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-march=armv8-a")
-set(CMAKE_CXX_FLAGS "-march=armv8-a")
+set(CMAKE_SYSROOT "/opt/petalinux/2021.2/sysroots/cortexa72-cortexa53-xilinx-linux")
+set(CMAKE_C_FLAGS "-mcpu=cortex-a72.cortex-a53 -march=armv8-a+crc -fstack-protector-strong  -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Werror=format-security -fno-rtti -fno-exceptions -flto -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -feliminate-unused-debug-types --sysroot=/opt/petalinux/2021.2/sysroots/cortexa72-cortexa53-xilinx-linux")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-a72.cortex-a53 -march=armv8-a+crc -fstack-protector-strong  -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Werror=format-security -fno-rtti -fno-exceptions -flto -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -feliminate-unused-debug-types --sysroot=/opt/petalinux/2021.2/sysroots/cortexa72-cortexa53-xilinx-linux")
 
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
